0ac066cca25b1a27f6be7038064fdff6e598ca6b
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-21 12:17:09 +0200

    Fix UI components export structure

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './Button'
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card'
 export { default as Container } from './Container'
+export { default as Card } from './Card'
